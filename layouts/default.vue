2d11da98ba96dcc986ca75f4f07a6559ed7a0f62
Sam Nau <sam.nau@vml.com>
2018-11-30 10:11:47 -0600

    more scaffolding for proof of concept

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h1>test</h1>
+    <img :src="imageContext('Savannah-cat-long-body-shot.jpg')">
     <nuxt/>
   </div>
 </template>
--- plugins/imageContext.js
+++ plugins/imageContext.js
@@ -1,10 +1,7 @@
 import Vue from 'vue';
 
 const imageHelper = require.context('~/assets/images', true, /\.(gif|png|jpe?g|svg)$/);
-const staticImageHelper = require.context('~/static/images', true, /\.(gif|png|jpe?g|svg)$/);
 const thumbnailHelper = require.context('~/assets/images/thumbnails', true, /\.(gif|png|jpe?g|svg)$/);
-const hotspotHelper = require.context('~/assets/images/hotspot_assets', true, /\.(gif|png|jpe?g|svg|mp4)$/);
-
 
 Vue.prototype.imageContext = (imagePath, isStatic) => {
   const targetHelper = isStatic ? staticImageHelper : imageHelper;
@@ -26,7 +23,3 @@ Vue.prototype.hotpsotContext = (imagePath) => {
   }
   return hotspotHelper(`./${imagePath}`);
 };
-
-if(imageHelper) {
-   imageHelper.forEach((x)=> { console.log(x)});
-}
